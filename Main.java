6e3ee889541f6f0c1da54aab299e06d95054eb82
tomad6909 <tomad6909@wrdsb.ca>
2020-09-15 14:03:41 +0000

    Finished A2Q1

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 /**
- *
+ * This program determines if a number that the user inputs is odd or even
  * @author 
  */
 public class Main {
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user
+    Scanner input = new Scanner(System.in);
+
+    // ask user for an integer
+    System.out.println("Please enter an integer:");
+   
+    int number = input.nextInt();
+
+    // detemine
+    if(number % 2 == 0){
+      System.out.println(number + " is an even number");
+    } else {
+      System.out.println(number + " is an odd number");
+    }
     
   }
 }
